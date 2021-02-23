f5db71522d2a190f144b7d12548fd2e24963bd43
liqiqiorz <1099462011@qq.com>
2021-02-23 13:47:31 +0800

    feat:Email and Storage config

--- eladmin-tools/src/main/java/me/zhengjie/repository/EmailRepository.java
+++ eladmin-tools/src/main/java/me/zhengjie/repository/EmailRepository.java
@@ -19,8 +19,8 @@ import me.zhengjie.domain.EmailConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * @author Zheng Jie
- * @date 2018-12-26
+ * @author YangXiangrui
+ * @date 2020-12-26
  */
 public interface EmailRepository extends JpaRepository<EmailConfig,Long> {
 }
